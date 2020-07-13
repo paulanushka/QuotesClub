31cf1873948f6525b18dfd8a1bd1532b242a712f
Anushka Paul <anushkapaul1103@gmail.com>
2020-07-13 14:59:43 +0530

    setting up to push to Heroku

--- app.js
+++ app.js
@@ -49,6 +49,5 @@ app.use("/",indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-app.listen(3000, function(){
-    console.log("server started");
-});
+const port = process.env.PORT || 3000;
+app.listen(port);
