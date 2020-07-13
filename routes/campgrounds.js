bc817ac09665c05484a4100f837dff405bf1ac63
Anushka Paul <anushkapaul1103@gmail.com>
2020-07-13 08:35:07 +0530

    new1

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -8,7 +8,7 @@ router.get("/", function(req, res) {
         if(err){
             console.log(err);
         } else {
-            res.render("campgrounds/index", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
     });
 });
